9354045076427108309f046d3c13bb5e9771a572
ArtyGeneralov <artyom1100@gmail.com>
2025-12-01 16:30:18 -0600

    Added win32 call for cursor position

---
+++ KeyboardGestures.UI/Utilities/Win32Cursor.cs
@@ -0,0 +1,24 @@
+﻿using Avalonia;
+using System.Runtime.InteropServices;
+
+namespace KeyboardGestures.UI.Utilities
+{
+    public static class Win32Cursor
+    {
+        [DllImport("user32.dll")]
+        private static extern bool GetCursorPos(out POINT lpPoint);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct POINT
+        {
+            public int X;
+            public int Y;
+        }
+
+        public static PixelPoint GetCursorPixelPoint()
+        {
+            GetCursorPos(out POINT p);
+            return new PixelPoint(p.X, p.Y);
+        }
+    }
+}
