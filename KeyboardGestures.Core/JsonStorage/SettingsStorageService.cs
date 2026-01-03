b11ad04e1f20f6752406d46efb7474f994e59f7f
ArtyGeneralov <artyom1100@gmail.com>
2026-01-03 06:20:53 -0600

    App settings storage service

---
+++ KeyboardGestures.Core/JsonStorage/SettingsStorageService.cs
@@ -0,0 +1,17 @@
+﻿using KeyboardGestures.Core.Settings;
+
+namespace KeyboardGestures.Core.JsonStorage
+{
+    public class SettingsStorageService
+    {
+        private readonly IJsonStorage<AppSettings> _storage;
+
+        public SettingsStorageService(IJsonStorage<AppSettings> storage)
+        {
+            _storage = storage;
+        }
+
+        public AppSettings Load() => _storage.Load();
+        public void Save(AppSettings appSettings) => _storage.Save(appSettings);
+    }
+}
